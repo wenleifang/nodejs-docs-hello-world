14b19a90b59edb7cd30d5b7120f5fc33e3a4c0c1
wenleifang <wenlei@hotmail.com>
2019-01-25 14:45:30 -0600

    Update index.js
    change port back to 1337

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = process.env.PORT || 8085;
+var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
