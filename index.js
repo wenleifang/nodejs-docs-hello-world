28b895805e7dd6ee13d7aff0f84fd14013992655
wenleifang <wenlei@hotmail.com>
2019-01-25 14:33:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! It's so cooold in Minnesota in January! You betcha!!!");
+    response.end("It's so cooold in Minnesota in January! You betcha!!!");
 
 });
 
